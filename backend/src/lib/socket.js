3ce6a3b5dc5f3c1293cdf4947ec4be1b8201cb29
Hir-diyansh-01 <your-github-email@gmail.com>
2026-02-24 16:06:26 +0530

    Fixed for production

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -2,12 +2,17 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
-const app = express();
-const server = http.createServer(app);
+export const app = express();
+export const server = http.createServer(app);
 
-const io = new Server(server, {
+export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://nex-chat-app-ywzb.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
+    import.meta.env.MODE === "production"
       ? "http://localhost:5001/api"
       : "https://nexchat-app-ohn7.onrender.com/api",
   withCredentials: true,
